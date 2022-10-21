f17de8e8b5b67636f9d5e9dfff11b3e068347ed1
RedRudy01 <rudolf.voros@stud.uni-corvinus.hu>
2022-10-21 11:12:09 +0200

    commit

--- gyak5/gyak5/Form1.cs
+++ gyak5/gyak5/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak5.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,21 @@ namespace gyak5
 {
     public partial class Form1 : Form
     {
+        MNBArfolyamServiceSoapClient mbservice = new MNBArfolyamServiceSoapClient();
+        GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+
         public Form1()
         {
             InitializeComponent();
+
+            var mnbService = new MNBArfolyamServiceSoapClient ();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
         }
     }
 }
