6e5fa1aa503bee244a3a22767841581e7eb89634
RedRudy01 <rudolf.voros@stud.uni-corvinus.hu>
2022-11-04 10:20:53 +0100

    commit

--- gyak6/WindowsFormsApp1/WindowsFormsApp1/Entities/Ball.cs
+++ gyak6/WindowsFormsApp1/WindowsFormsApp1/Entities/Ball.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WindowsFormsApp1.Entities
 {
-    public class Ball: Label
+    public class Ball: System.Windows.Forms.Label
     {
         public Ball()
         {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Ball_Paint;
+        }
 
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
