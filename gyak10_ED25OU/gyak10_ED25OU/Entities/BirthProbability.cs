f97d5efbd093e8c935750e4aea0d571d3d04abe6
RedRudy01 <rudolf.voros@stud.uni-corvinus.hu>
2022-12-02 10:33:42 +0100

    commit

---
+++ gyak10_ED25OU/gyak10_ED25OU/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak10_ED25OU.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double BirthProb { get; set; }
+
+    }
+}
---
+++ gyak10_ED25OU/gyak10_ED25OU/Entities/DeathProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak10_ED25OU.Entities
+{
+    public class DeathProbability
+    {
+        public Gender gender { get; set; }
+        public int Age { get; set; }
+
+        public double DeathProb { get; set; }
+
+    }
+}
